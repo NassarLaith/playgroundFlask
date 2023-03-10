684f41a9f457861e67aca95f1efa0e65822eee84
NassarLaith <Nassarlaith.9@gmail.com>
2023-03-09 19:20:36 -0500

    secondCommit

--- server.py
+++ server.py
@@ -17,5 +17,7 @@ def wrong_path(path):
 
 
 
+
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
